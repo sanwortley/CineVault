54b7451c54bd0ee58dd2a3c3c7cbf40da1065c79
Santi <sanwortley@gmail.com>
2026-05-07 14:20:12 -0300

    Fix slider isDragging state getting stuck when mouse released outside

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1140,11 +1140,9 @@ function VideoPlayer({ movie, onClose, onOpenSettings, onVersionChange, userProg
                                     min="0"
                                     max={effectiveDuration || 100}
                                     value={clampedTime}
-                                    onMouseDown={handleSeekStart}
-                                    onTouchStart={handleSeekStart}
+                                    onPointerDown={handleSeekStart}
                                     onChange={handleSeekChange}
-                                    onMouseUp={handleSeekEnd}
-                                    onTouchEnd={handleSeekEnd}
+                                    onPointerUp={handleSeekEnd}
                                     style={{ background: `linear-gradient(to right, #06b6d4 ${progressPct}%, rgba(255,255,255,0.2) ${progressPct}%)` }}
                                     className="w-full h-1 rounded-full appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-cyan-500 [&::-webkit-slider-thumb]:shadow-lg"
                                 />
